2165efe7bd2c2be3538ab51325d43e5fdfc273d6
ZenkosClub <zenkosclub@gmail.com>
2025-08-16 20:24:11 -0500

    Update admin-tag.js

--- plugins/admin-tag.js
+++ plugins/admin-tag.js
@@ -1,26 +1,42 @@
-let handler = async (m, { conn, args, isAdmin, isOwner, isPrems }) => {
+let handler = async (m, { conn, text, isAdmin, isOwner, isPrems }) => {
 
-  if (!m.isGroup) return
-  if (!isAdmin && !isOwner && !isPrems) return conn.sendMessage(m.chat, {
-    text: '《✩》Solo los administradores pueden usar este comando.',
-    contextInfo: { ...m.contextInfo }
+  if (!m.isGroup) return conn.sendMessage(m.chat, {
+    text: '*[❗] Este comando solo puede ser usado en grupos.*',
+    contextInfo: {
+      ...rcanal.contextInfo
+    }
   }, { quoted: m })
-
+  
+  if (!isAdmin && !isOwner && !isPrems) return conn.sendMessage(m.chat, {
+    text: '*[❗] Solo los administradores pueden usar este comando.*',
+    contextInfo: {
+      ...rcanal.contextInfo
+    }
+  }, { quoted: m })
+  
   try {
+
     const groupMetadata = await conn.groupMetadata(m.chat)
-    const participants = groupMetadata.participants.map(p => p.id)
-
-    await conn.sendMessage(m.chat, {
-      text: args?.join(' ') || ' ',
-      contextInfo: { mentionedJid: participants }
+    const participants = groupMetadata.participants
+    
+    await conn.sendMessage(m.chat, { 
+      text: text || ' ',
+      mentions: participants.map(p => p.id)
     }, { quoted: m })
-
+    
   } catch (e) {
-    console.error(e)
+    console.error('Error en comando tag-all:', e)
+    conn.sendMessage(m.chat, {
+      text: '*[❗] Ocurrió un error al intentar etiquetar a los miembros del grupo.*',
+      contextInfo: {
+        ...rcanal.contextInfo
+      }
+    }, { quoted: m })
   }
 }
 
-handler.command = ['tag', 'say', 'tagall']
-handler.help = ['tag']
-handler.tags = ['admin']
+handler.help = ['#tag']
+handler.tags = ['grupos']
+handler.command = ['tag', 'todos', 'mencionartodos']
+
 export default handler
