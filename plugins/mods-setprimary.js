b18f1cf1665f710a9c330ee63d033ca946bdc22b
ZenkosClub <zenkosclub@gmail.com>
2025-08-16 19:06:51 -0500

    Update mods-setprimary.js

--- plugins/mods-setprimary.js
+++ plugins/mods-setprimary.js
@@ -1,13 +1,14 @@
 import fs from 'fs'
 import path from 'path'
+import { join } from 'path'
 
-let handler = async (m, { text, isAdmin, isOwner, isPrems }) => {
+let handler = async (m, { conn, text, isAdmin, isOwner, isPrems }) => {
   if (!isAdmin && !isOwner && !isPrems) {
-    return m.reply('《✧》Solo los administradores pueden usar este comando.')
+    return conn.reply(m.chat, '《✧》Solo los administradores pueden usar este comando.', m)
   }
 
   if (!text || !text.replace(/[^0-9]/g, '')) {
-    return m.reply('Debes etiquetar o escribir el número del bot que quieres hacer principal en este grupo.')
+    return conn.reply(m.chat, `《✧》Debes escribir o mencionar el número del bot que quieres establecer como primario.\n\n> Ejemplo: #setprimary 51987654321`, m)
   }
 
   let number = text.replace(/[^0-9]/g, '')
@@ -15,20 +16,17 @@ let handler = async (m, { text, isAdmin, isOwner, isPrems }) => {
   let subbotPath = path.join('./serbots', number, 'creds.json')
 
   if (!fs.existsSync(subbotPath)) {
-    return m.reply(`El número *${number}* no corresponde a un Subbot válido (no se encontró su *creds.json* en JadiBots).`)
+    return conn.reply(m.chat, `El número *${number}* no corresponde a un Subbot válido (no se encontró *creds.json* en ./serbots).`, m)
   }
 
   if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
   global.db.data.chats[m.chat].primaryBot = botJid
 
-  return m.reply(
-    `✿ Bot establecido como primario en este grupo.\n\n✰ Bot: *@${number}*\n❏ Admin: @${m.sender.split('@')[0]}`, 
-    { mentions: [botJid, m.sender] }
-  )
+  return conn.reply(m.chat, `✿ Bot establecido como primario en este grupo.\n\n✰ Bot: *@${number}*\n❏ Admin: @${m.sender.split('@')[0]}`, m, { mentions: [botJid, m.sender] })
 }
 
-handler.help = ['setprimary @bot | número']
-handler.tags = ['serbot']
 handler.command = ['setprimary']
+handler.help = ['setprimary']
+handler.tags = ['socket']
 
 export default handler
