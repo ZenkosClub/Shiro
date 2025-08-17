f1899089f915c44d104b8968cab8fa307dae56bf
ZenkosClub <zenkosclub@gmail.com>
2025-08-16 20:31:44 -0500

    Update admin-tag.js

--- plugins/admin-tag.js
+++ plugins/admin-tag.js
@@ -1,24 +1,28 @@
-let handler = async (m, { conn, args, isAdmin, isOwner, isPrems }) => {
+let handler = async (m, { conn, text, isAdmin, isOwner, isPrems }) => {
 
-  if (!m.isGroup) return
-  if (!isAdmin && !isOwner && !isPrems) return conn.sendMessage(m.chat, {
-    text: '《✩》Solo los administradores pueden usar este comando.',
-    contextInfo: { ...m.contextInfo }
+  if (!m.isGroup) return conn.sendMessage(m.chat, {
+    text: '*[❗] Este comando solo puede ser usado en grupos.*',
+    contextInfo: {
+      ...rcanal.contextInfo
+    }
   }, { quoted: m })
-
+  
+  if (!isAdmin && !isOwner && !isPrems) return conn.sendMessage(m.chat, {
+    text: '《✩》Solo los administradores pueden usar este comando.*',
+    contextInfo: {
+      ...rcanal.contextInfo
+    }
+  }, { quoted: m })
+  
   try {
+
     const groupMetadata = await conn.groupMetadata(m.chat)
     const participants = groupMetadata.participants
-
-    await conn.sendMessage(m.chat, {
-      text: args?.join(' ') || ' ',
+    
+    await conn.sendMessage(m.chat, { 
+      text: text || ' ',
       mentions: participants.map(p => p.id)
     }, { quoted: m })
-
-  } catch (e) {
-    console.error(e)
-  }
-}
 
 handler.command = ['tag', 'say', 'tagall']
 handler.help = ['tag']
