60adc28a7f86c95ea52b900a7d95b641957c7e51
ZenkosClub <zenkosclub@gmail.com>
2025-08-20 16:07:07 -0500

    Update boss-setimage.js

--- plugins/boss-setimage.js
+++ plugins/boss-setimage.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, isAdmin }) => {
 
   if (!mime || !(mime.startsWith('image/') || mime === 'image/webp')) {
     return conn.sendMessage(m.chat, { 
-      text: 'ᰔᩚ Acción inválida.\n> ꕥ Debes *responder* a una imagen o sticker con el comando.\n\nꕥ _Ejemplo:_\n> ᰔᩚ *#groupimage (respondiendo a una foto o sticker)*', 
+      text: 'ᰔᩚ Acción inválida.\n> ꕥ Debes *responder* a una imagen o sticker.', 
       contextInfo: { ...(m.contextInfo || {}) } 
     }, { quoted: m })
   }
@@ -33,5 +33,4 @@ let handler = async (m, { conn, isAdmin }) => {
 handler.command = ['groupimage', 'gbimg', 'imagegb', 'setgroupimg']
 handler.help = ['groupimage']
 handler.tags = ['boss']
-
 export default handler
